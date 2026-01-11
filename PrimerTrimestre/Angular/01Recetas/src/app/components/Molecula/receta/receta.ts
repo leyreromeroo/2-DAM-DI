998cf723a806be1fc7e4d4f03a58bb81d63caf1e
Leyre Romero Velasco <leyre.romero@gmail.com>
2026-01-11 20:55:06 +0100

    feat(frontend): actualizar componentes de visualización y votación

--- PrimerTrimestre/Angular/01Recetas/src/app/components/Molecula/receta/receta.ts
+++ PrimerTrimestre/Angular/01Recetas/src/app/components/Molecula/receta/receta.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input} from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { RecetaModel } from '../../../models/recetaModel';
 import { CommonModule } from '@angular/common';
 import { ServicioRecetas } from '../../../services/servicio-recetas';
@@ -11,19 +11,18 @@ import { ServicioRecetas } from '../../../services/servicio-recetas';
   styleUrl: './receta.scss'
 })
 export class Receta {
-  
+
   public readonly Math = Math;
-  constructor(private recetasService: ServicioRecetas) {}
+  constructor(private recetasService: ServicioRecetas) { }
 
   //@Input() recetaModel!: RecetaModel;
   recetaModel = input.required<RecetaModel>();
 
   onEliminar() {
-    //Al utilizar la forma más nueva de Input, se vuelve recetaModel() en una función y se accede a ella con ()
     this.recetasService.borrarReceta(this.recetaModel().id);
   }
 
   onVotar(puntos: number) {
-    this.recetasService.valorarReceta(this.recetaModel(), puntos);
+    this.recetasService.valorarReceta(this.recetaModel().id, puntos);
   }
 }
